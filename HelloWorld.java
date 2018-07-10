200991eb0f9015cd35910b2d1e46432fa9677571
fabian152195 <38987619+fabian152195@users.noreply.github.com>
2018-07-09 18:02:57 -0600

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,38 @@
+
+
+/**
+ * tipo de commentario multilinea
+ * dls
+ */
+
+
+/*
+otro tipo de
+comentario multilinea
+ */
+
+// comentario unilinea
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.print("Hi, what's your name? ");
+        Scanner input = new Scanner(System.in);
+        name = input.nextLine();
+        System.out.print("Greetings " + name + " nice to meet you! Where are you planning to travel? ");
+        place = input.nextLine();
+        System.out.println("Great! "+ place + " sounds like a great place");
+
+
+        //int y = (int) 8.2;
+        //int x = 10;
+
+
+        Scanner console = new Scanner(System.in);
+        System.out.print("How much money do you have? ");
+        double money = console.nextDouble();
+    }
+
+}
